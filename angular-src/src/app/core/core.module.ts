f6c6d16a7de06b3e10ec10088a6ba1ea4d9d65ac
Shynet <shynet@gmail.com>
2018-12-11 22:41:24 +0200

    Added ngx-toastr

--- angular-src/src/app/core/core.module.ts
+++ angular-src/src/app/core/core.module.ts
@@ -3,15 +3,15 @@ import { AuthService } from './services/auth.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { ToastyModule } from 'ng2-toasty';
+import { ToastrModule } from 'ngx-toastr';
 import { CookieModule } from 'ngx-cookie';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import {
   ApiService,
   AppService,
-  ToastyHelperService,
   RequestsService
 } from './services';
 import { SharedModule } from '../shared/shared.module';
@@ -25,9 +25,13 @@ import { SocialLoginModule } from '../social-login/social-login.module';
   imports: [
     CommonModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     CookieModule.forRoot(),
     SlimLoadingBarModule.forRoot(),
-    ToastyModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-bottom-right'
+    }) ,
     RouterModule,
     SharedModule,
     SocialLoginModule
@@ -38,7 +42,6 @@ import { SocialLoginModule } from '../social-login/social-login.module';
     AuthService,
     AuthGuardService,
     AppService,
-    ToastyHelperService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AppHttpInterceptor,
@@ -50,7 +53,7 @@ import { SocialLoginModule } from '../social-login/social-login.module';
     HeaderComponent,
     FooterComponent,
     SlimLoadingBarModule,
-    ToastyModule,
+    ToastrModule,
     SocialLoginModule
   ]
 })
--- angular-src/src/app/core/services/index.ts
+++ angular-src/src/app/core/services/index.ts
@@ -2,5 +2,4 @@ export { AuthGuardService } from './auth-guard.service';
 export { AppService } from './app.service';
 export { ApiService } from './api.service';
 export { AuthService } from './auth.service';
-export { ToastyHelperService } from './toasty-helper.service';
 export { RequestsService } from './requests.service';
