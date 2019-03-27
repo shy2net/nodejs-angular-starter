f10a07c381676c98ad5939175a74475752b4a024
Shynet <shynet@gmail.com>
2019-03-27 23:58:29 +0200

    Moved to scss in app.component

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -18,7 +18,7 @@ declare var jQuery: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(
