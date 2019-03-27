654f1d2d3641b956dcbd1a79135bd17eb1eeee65
Shynet <shynet@gmail.com>
2019-03-28 00:12:28 +0200

    Updated build script to include environments

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # This script compiles typescript and Angular 5 application and puts them into a single NodeJS prject
-echo "-- Started build script for Angular & NodeJS --"
+ENV=${NODE_ENV:-development}
+echo "-- Started build script for Angular & NodeJS (environment $ENV) --"
 echo "Removing out directory..."
 rm -rf out
 
@@ -14,8 +15,8 @@ cp -Rf src/config out/src/config
 echo "Installing Angular app dependencies..."
 cd angular-src && npm install --only=dev
 
-echo "Building Angular app for distribution..."
-./node_modules/.bin/ng build --aot --prod
+echo "Building Angular app for $ENV..."
+./node_modules/.bin/ng build --aot --prod --configuration $ENV
 
 echo "Building Angular app for SSR..."
 ./node_modules/.bin/ng run angular-src:server:production && ./node_modules/.bin/webpack --config webpack.server.config.js --progress --colors
