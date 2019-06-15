d5d53adb7ebe97c4574a608773b4192ab629b035
Shay Yaish <shynet@gmail.com>
2019-06-15 15:42:37 +0300

    Changed files according to new template

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 # This script compiles typescript and Angular 7 application and puts them into a single NodeJS project
 ENV=${NODE_ENV:-development}
 echo "-- Started build script for Angular & NodeJS (environment $ENV) --"
-echo "Removing out directory..."
-rm -rf out
+echo "Removing dist directory..."
+rm -rf dist
 
 echo "Compiling typescript..."
 ./node_modules/.bin/tsc -p .
 
 echo "Copying configuration files..."
-cp -Rf src/config out/src/config
+cp -Rf src/config dist/src/config
 
 echo "Installing Angular app dependencies..."
 cd angular-src && npm install --only=dev
@@ -18,19 +18,17 @@ cd angular-src && npm install --only=dev
 echo "Building Angular app for $ENV..."
 ./node_modules/.bin/ng build --aot --prod --configuration $ENV
 
-
 # TODO: Remove this 'if' statment until the 'fi' if you don't want SSR at all
-if [ $ENV == "production" ]
-then
+if [ $ENV == "production" ]; then
     echo "Building Angular app for SSR..."
     ./node_modules/.bin/ng run angular-src:server:production && ./node_modules/.bin/webpack --config webpack.server.config.js --progress --colors
 else
     echo "Skipping build for SSR as environment is NOT production"
 fi
 
-echo "Copying angular dist into out directory..."
-mkdir ../out/src/dist
-cp -Rf dist ../out/src
+echo "Copying angular dist into dist directory..."
+mkdir ../dist/src/dist
+cp -Rf dist ../dist/src
 
 echo "Removing angular-src dist directory..."
 rm -rf dist
@@ -38,4 +36,4 @@ rm -rf dist
 # Go back to the current directory
 cd ..
 
-echo "-- Finished building Angular & NodeJS, check out directory --"
+echo "-- Finished building Angular & NodeJS, check dist directory --"
---
+++ predebug.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Building typescript..."
+
+rm -rf ./dist
+./node_modules/.bin/tsc -p .
+
+echo "Copying configuration files..."
+rm -rf ./dist/src/config
+mkdir ./dist/src/config
+cp -Rf ./src/config/* ./dist/src/config
+echo "Configuration files succesfully copied!"
+echo "Ready for debugging!"
