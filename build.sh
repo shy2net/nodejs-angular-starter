36d86059e2b9dfaa9f7f881eb0fff205a772edc1
Shynet <shynet@gmail.com>
2020-02-29 19:22:45 +0200

    fix(build.sh): improved build script to address compilation issues and abort build

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Echo colors
+red=$(tput setaf 1)
+
+check_errcode() {
+    status=$?
+
+    if [ $status -ne 0 ]; then
+        echo "${red}${1}"
+        exit $satus
+    fi
+}
+
 echo "Checking for missing dependencies before build..."
 
 # Check if node_modules exists, if not throw an error
@@ -12,27 +24,31 @@ else
 fi
 
 # This script compiles typescript and Angular 7 application and puts them into a single NodeJS project
-ENV=${NODE_ENV:-development}
+ENV=${NODE_ENV:-production}
 echo -e "\n-- Started build script for Angular & NodeJS (environment $ENV) --"
 echo "Removing dist directory..."
 rm -rf dist
 
 echo "Compiling typescript..."
 ./node_modules/.bin/tsc -p .
+check_errcode "Failed to compile typescript! aborting script!"
 
 echo "Copying configuration files..."
 cp -Rf src/config dist/src/config
+check_errcode "Failed to copy configuration files! aborting script!"
 
 echo "Starting to configure Angular app..."
 pushd angular-src
 
 echo "Building Angular app for $ENV..."
 ./node_modules/.bin/ng build --aot --prod --configuration $ENV
+check_errcode "Failed to build angular! stopping script!"
 
 # TODO: Remove this 'if' statment until the 'fi' if you don't want SSR at all
 if [ $ENV == "production" ]; then
     echo "Building Angular app for SSR..."
     ./node_modules/.bin/ng run angular-src:server:production && ./node_modules/.bin/webpack --config webpack.server.config.js --progress --colors
+    check_errcode "Failed to build Angular app for SSR! aborting script!"
 else
     echo "Skipping build for SSR as environment is NOT production"
 fi
@@ -40,6 +56,7 @@ fi
 echo "Copying angular dist into dist directory..."
 mkdir ../dist/src/dist
 cp -Rf dist ../dist/src
+check_errcode "Failed to copy anuglar dist files! aborting script!"
 
 echo "Removing angular-src dist directory..."
 rm -rf dist
